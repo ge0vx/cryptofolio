60a01c5143d8f8f81ac0245e68f3ebb57c768a34
giovanyperezbautista <geovapb@gmail.com>
2021-04-09 18:59:44 -0500

    basic configuration template

---
+++ src/store.js
@@ -0,0 +1,15 @@
+// @flow
+
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+const middlewares = [thunk];
+
+if(__DEV__) {
+    middlewares.push(createLogger());
+}
+
+export default createStore(rootReducer, undefined, composeWithDevTools(applyMiddleware(...middlewares)));
